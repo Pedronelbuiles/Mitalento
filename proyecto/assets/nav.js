936613bb604e42e942377bcf80d9a7bb0b4e6bfe
Pedronelbuiles <pedronel.builes@gmail.com>
2017-08-28 10:57:36 -0500

    comenzar a hacer la parte de dep-sis

--- proyecto/assets/nav.js
+++ proyecto/assets/nav.js
@@ -1,6 +1,12 @@
 var  mn = $(".main-nav");
     mns = "main-nav-scrolled";
     hdr = $(window).height()+10;
+    imgt = $(".img_team");
+    imgent= "animated bounceInLeft visible";
+    imgnt = $(".nosotos_team");
+    imgntent= "animated bounceInRight visible";
+    entrada = hdr - 130;
+    entrada_dos = hdr + 600 ;
 
 $(window).scroll(function() {
   if( $(this).scrollTop() > hdr ) {
@@ -10,3 +16,17 @@ $(window).scroll(function() {
   }
 });
 
+$(window).scroll(function() {
+  if( $(this).scrollTop() > entrada ) {
+    imgt.addClass(imgent);
+  } else {
+    imgt.removeClass(imgent);
+  }
+});
+
+$(window).scroll();
+
+console.log(hdr);
+console.log(entrada);
+console.log(entrada_dos);
+console.log($(window).scrollTop());
